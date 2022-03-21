8c8d47f65eff43512277feb04a7b9e7f9fdfb23b
Benoit Auclair <benoitauclair30@gmail.com>
2022-03-21 10:49:08 +0100

    Changes to computeJacobian
    The method was moved from class KalmanFilter to MotionModel
    The Jacobians are computed based on analytical solution (no longer numerical) to the partial derivatives
    Launched the node and tested in the environment and it seems to work just fine

--- scripts/Localization.py
+++ scripts/Localization.py
@@ -73,6 +73,15 @@ class MotionModel:
         self.dt = dt
         self.noise_model = NoiseModel()
 
+        # initialization of the Jacobians
+        self.jacobian_G = np.zeros((3, 3))
+        self.jacobian_G[0, 0] = 1
+        self.jacobian_G[1, 1] = 1
+        self.jacobian_G[2, 2] = 1
+
+        self.jacobian_V = np.zeros((3, 2))
+        # self.threshold_div_zero = 1e-2
+
     def predictPose(self, control_input, last_pose):
         """
         This method updates the predicted robot pose.
@@ -94,12 +103,33 @@ class MotionModel:
         print("increment :", increment)
         next_pose = last_pose + increment.reshape(3, 1)  # was shape (3,1,1) which lead to (3,3,1)
         print("next pose :", next_pose)
-        # NOTE: let's start debugging without any error
-        # self.next_error has been intialized to 0
+
         next_error = self.noise_model.estimateError(v, w)
 
-        return next_pose, next_error
+        self.computeJacobian(v, w, last_pose)
 
+        return next_pose, next_error, self.jacobian_G, self.jacobian_V
+
+    def computeJacobian(self, v, w, last_pose):
+        """
+        This method computes the Jacobians of the kinematic model.
+        @param:
+            *v: linear speed w.r.t. x-axis in robot frame
+            *w: angular speed w.r.t. z-axis in robot frame
+        @result:
+            *self.jacobian_G: Jacobian with respect to the state estimate
+            *self.jacobian_V: Jacobian with respect to the control inputs
+        """
+
+        self.jacobian_G[0, 2] = -1 * v * self.dt * np.sin(last_pose[2] + w * self.dt / 2)
+        self.jacobian_G[1, 2] = v * self.dt * np.cos(last_pose[2] + w * self.dt / 2)
+
+        self.jacobian_V[0, 0] = self.dt * np.cos(last_pose[2] + w * self.dt / 2)
+        self.jacobian_V[0, 1] = -1 * v * np.power(self.dt, 2) * 0.5 * np.sin(last_pose[2] + w * self.dt / 2)
+
+        self.jacobian_V[1, 0] = self.dt * np.sin(last_pose[2] + w * self.dt / 2)
+        self.jacobian_V[1, 1] = v * np.power(self.dt, 2) * 0.5 * np.cos(last_pose[2] + w * self.dt / 2)
+        self.jacobian_V[2, 1] = self.dt
 
 class KalmanFilter:
     """
@@ -128,12 +158,6 @@ class KalmanFilter:
         # robot doesn't move at t = 0
         self.last_control_input = np.zeros((2, 1))
 
-        # initialization
-        self.jacobian_G = np.zeros((3, 3))
-        self.jacobian_V = np.zeros((3, 2))
-        self.threshold_div_zero = 1e-2
-        self.next_state_mu = np.zeros((3, 1))
-
     def predict(self, control_input):
         """
         This method predicts what the next system state will be.
@@ -146,86 +170,87 @@ class KalmanFilter:
         """
 
         # compute the next state i.e. next robot pose knowing current control inputs
-        self.next_state_mu, next_error = self.motion_model.predictPose(control_input, self.last_state_mu)
-        print("jac V:", self.jacobian_V)
+        next_state_mu, next_error, jacobian_G, jacobian_V = self.motion_model.predictPose(control_input, self.last_state_mu)
+        print("jac V:", jacobian_V)
+        print("jac G:", jacobian_G)
         print("next error:", next_error)
-        # compute the jacobians necessary for the EKF prediction
-        self.computeJacobian(control_input)
+        print("next state:", next_state_mu)
 
         # compute covariance on the state transition probability
-        covariance_R = self.jacobian_V @ next_error @ self.jacobian_V.T
-        next_covariance = self.jacobian_G @ self.last_covariance @ self.jacobian_G.T + covariance_R
+        covariance_R = jacobian_V @ next_error @ jacobian_V.T
+        next_covariance = jacobian_G @ self.last_covariance @ jacobian_G.T + covariance_R
         print("next_covariance :", next_covariance)
 
         # store current state estimate, current covariance on prior belief, current control inputs
         # for use in the next iteration
-        self.last_state_mu = self.next_state_mu
-
+        self.last_state_mu = next_state_mu
         self.last_covariance = next_covariance
-
         self.last_control_input = control_input
 
-        return self.next_state_mu, next_covariance
+        return next_state_mu, next_covariance
 
-    def computeJacobian(self, control_input):
-        """
-        This method computes the Jacobians of the kinematic model.
-        @param: control_input - numpy array of dim 2 x 1 containing:
-            *linear speed w.r.t. x-axis in robot frame, v
-            *angular speed w.r.t. z-axis in robot frame, w
-        @result:
-            *self.jacobian_G: Jacobian with respect to the state estimate
-            *self.jacobian_V: Jacobian with respect to the control inputs
-        """
 
-        delta_g = self.next_state_mu - self.last_state_mu
-        delta_x = np.array(self.next_state_mu[0, 0] - self.last_state_mu[0, 0])
-        delta_y = np.array(self.next_state_mu[1, 0] - self.last_state_mu[1, 0])
-        delta_psi = np.array(self.next_state_mu[2, 0] - self.last_state_mu[2, 0])
+        #######################
+        #Compute the Jacobians v1
+        #######################
 
-        print("delta_g:", delta_g)
-        print("delta_x:", delta_x)
-        print("delta_y:", delta_y)
-        print("delta_psi:", delta_psi)
-        # we should make sure we don't divide by zero
-        if delta_x.all() > self.threshold_div_zero:
-            self.jacobian_G[:, 0] = np.array(delta_g / delta_x).reshape(3, )
-        else:
-            #             self.jacobian_G[:, 0] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
-            self.jacobian_G[:, 0] = 0
+    # def computeJacobian(self, control_input):
+    #     """
+    #     This method computes the Jacobians of the kinematic model.
+    #     @param: control_input - numpy array of dim 2 x 1 containing:
+    #         *linear speed w.r.t. x-axis in robot frame, v
+    #         *angular speed w.r.t. z-axis in robot frame, w
+    #     @result:
+    #         *self.jacobian_G: Jacobian with respect to the state estimate
+    #         *self.jacobian_V: Jacobian with respect to the control inputs
+    #     """
 
-        if delta_y.all() > self.threshold_div_zero:
-            self.jacobian_G[:, 1] = np.array(delta_g / delta_y).reshape(3, )
-        else:
-            #             self.jacobian_G[:, 1] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
-            self.jacobian_G[:, 1] = 0
+        # delta_g = self.next_state_mu - self.last_state_mu
+        # delta_x = np.array(self.next_state_mu[0, 0] - self.last_state_mu[0, 0])
+        # delta_y = np.array(self.next_state_mu[1, 0] - self.last_state_mu[1, 0])
+        # delta_psi = np.array(self.next_state_mu[2, 0] - self.last_state_mu[2, 0])
+        #
+        # print("delta_g:", delta_g)
+        # print("delta_x:", delta_x)
+        # print("delta_y:", delta_y)
+        # print("delta_psi:", delta_psi)
 
-        if delta_psi.all() > self.threshold_div_zero:
-            self.jacobian_G[:, 2] = np.array(delta_g / delta_psi).reshape(3, )
-        else:
-            #             self.jacobian_G[:, 2] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
-            self.jacobian_G[:, 2] = 0
-
-        delta_v = control_input[0] - self.last_control_input[0]
-        delta_w = control_input[1] - self.last_control_input[1]
-
-        print("delta_v:", delta_v)
-        print("delta_w:", delta_w)
-
-        if delta_v.all() > self.threshold_div_zero:
-            self.jacobian_V[:, 0] = np.array(delta_g / delta_v).reshape(3, )
-        else:
-            #             self.jacobian_V[:, 0] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
-            self.jacobian_V[:, 0] = 0
-
-        if delta_w.all() > self.threshold_div_zero:
-            self.jacobian_V[:, 1] = np.array(delta_g / delta_w).reshape(3, )
-        else:
-            #             self.jacobian_V[:, 1] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
-            self.jacobian_V[:, 1] = 0
-
-        print("jac G:", self.jacobian_G)
-        print("jac V:", self.jacobian_V)
+        # # we should make sure we don't divide by zero
+        # if delta_x.all() > self.threshold_div_zero:
+        #     self.jacobian_G[:, 0] = np.array(delta_g / delta_x).reshape(3, )
+        # else:
+        #     #             self.jacobian_G[:, 0] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
+        #     self.jacobian_G[:, 0] = 0
+        #
+        # if delta_y.all() > self.threshold_div_zero:
+        #     self.jacobian_G[:, 1] = np.array(delta_g / delta_y).reshape(3, )
+        # else:
+        #     #             self.jacobian_G[:, 1] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
+        #     self.jacobian_G[:, 1] = 0
+        #
+        # if delta_psi.all() > self.threshold_div_zero:
+        #     self.jacobian_G[:, 2] = np.array(delta_g / delta_psi).reshape(3, )
+        # else:
+        #     #             self.jacobian_G[:, 2] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
+        #     self.jacobian_G[:, 2] = 0
+        #
+        # delta_v = control_input[0] - self.last_control_input[0]
+        # delta_w = control_input[1] - self.last_control_input[1]
+        #
+        # print("delta_v:", delta_v)
+        # print("delta_w:", delta_w)
+        #
+        # if delta_v.all() > self.threshold_div_zero:
+        #     self.jacobian_V[:, 0] = np.array(delta_g / delta_v).reshape(3, )
+        # else:
+        #     #             self.jacobian_V[:, 0] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
+        #     self.jacobian_V[:, 0] = 0
+        #
+        # if delta_w.all() > self.threshold_div_zero:
+        #     self.jacobian_V[:, 1] = np.array(delta_g / delta_w).reshape(3, )
+        # else:
+        #     #             self.jacobian_V[:, 1] = np.array(delta_g / self.threshold_div_zero).reshape(3, )
+        #     self.jacobian_V[:, 1] = 0
 
 
 class Localization:
